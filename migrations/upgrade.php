28726d162b38dda2f01f0d566303c88bfae1d580
darkfriend <maslov.tlt@gmail.com>
2020-11-18 15:17:21 +0300

    added upgrade.php

---
+++ migrations/upgrade.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Upgrade script payment in php_interface
+ * User: darkfriend <hi@darkfriend.ru>
+ * Date: 08.09.2020
+ * Time: 23:28
+ */
+
+require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+
+\Bitrix\Main\Loader::includeModule('main');
+
+CopyDirFiles(
+    $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/dev2fun.stripepayment/install/sale_payment',
+    $_SERVER['DOCUMENT_ROOT'] . '/bitrix/php_interface/include/sale_payment',
+    true,
+    true
+);
+
+die("Upgrade - Success");
